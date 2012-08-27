07c7efce6e87cb7d87d615f1b57cae28902bdaab
sdepold <sascha@depold.com>
2012-08-27 09:07:30 +0200

    use new build script

--- utils/check_size.js
+++ utils/check_size.js
@@ -3,39 +3,27 @@ var execSync = require('exec-sync')
 var exec = function(cmd, showStdOut) {
   console.log(cmd)
 
-  var result = execSync(cmd)
+  var result = execSync(cmd, true)
 
   if(showStdOut) {
     console.log(result)
   }
 
-  return result
+  return result.stdout
 }
 
 exec('rm -rf ' + __dirname + '/tmp')
 exec('rm -rf ' + __dirname + '/tmp.zip')
 
-exec('mkdir -p ' + __dirname + '/tmp')
-exec('cp -rf ' + __dirname + '/../images ' + __dirname + '/tmp/images')
-exec('cp -rf ' + __dirname + '/../javascripts ' + __dirname + '/tmp/javascripts')
-exec('cp -rf ' + __dirname + '/../stylesheets ' + __dirname + '/tmp/stylesheets')
-exec('cp -rf ' + __dirname + '/../index.html ' + __dirname + '/tmp/index.html')
+exec('node ' + __dirname + '/build.js')
 
-exec('ls -l ' + __dirname + '/tmp/javascripts/*').split(/\n/).forEach(function(row) {
-  var source = __dirname + '/' + row.match(/(tmp.*)/)[1]
-    , target = source + '.2'
-    , cmd    = __dirname + '/node_modules/.bin/uglifyjs -mt --unsafe -o ' + source + ' ' + source
-
-  exec(cmd)
-})
-
-exec('zip -r -9 ' + __dirname + '/tmp.zip ' + __dirname + '/tmp')
+exec('zip -r -9 ' + __dirname + '/tmp.zip ' + __dirname + '/dist')
 
 var size = exec('ls -ila ' + __dirname + '/tmp.zip').split(/ /g).filter(function(i) {
   return i !== ''
 })[5]
 
-exec('rm -rf ' + __dirname + '/tmp')
-exec('rm -rf ' + __dirname + '/tmp.zip')
+exec('rm -rf ' + __dirname + '/dist')
+// exec('rm -rf ' + __dirname + '/tmp.zip')
 
 console.log('Size of zipfile: ', size)

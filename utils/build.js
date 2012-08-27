0ab74c009bdda6c372e937f7d3edd488f5512e13
sdepold <sascha@depold.com>
2012-08-27 11:34:12 +0200

    improved build script

--- utils/build.js
+++ utils/build.js
@@ -1,4 +1,7 @@
 var execSync = require('exec-sync')
+  , fs       = require('fs')
+  , jsp      = require("uglify-js").parser
+  , jsu      = require("uglify-js").uglify
 
 var exec = function(cmd, showStdOut) {
   console.log(cmd)
@@ -17,16 +20,22 @@ exec('rm -rf ' + __dirname + '/dist')
 exec('mkdir -p ' + __dirname + '/dist')
 
 exec('cp -rf ' + __dirname + '/../images ' + __dirname + '/dist/images')
-exec('cp -rf ' + __dirname + '/../javascripts ' + __dirname + '/dist/javascripts')
 exec('cp -rf ' + __dirname + '/../stylesheets ' + __dirname + '/dist/stylesheets')
 exec('cp -rf ' + __dirname + '/../index.html ' + __dirname + '/dist/index.html')
 
-exec('ls -l ' + __dirname + '/dist/javascripts/*').split(/\n/).forEach(function(row) {
-  var path = __dirname + '/' + row.match(/(dist.*)/)[1]
-    , cmd    = __dirname + '/node_modules/.bin/uglifyjs -mt --unsafe -o ' + path + ' ' + path
+var files    = ['utils', 'grid', 'grid-cell', 'monster', 'player', 'tower', 'tower-menu', 'tower-meta-menu', 'game', 'boot']
+  , paths    = files.map(function(file) { return __dirname + '/../javascripts/' + file + '.js' })
+  , contents = paths.map(function(path) { return exec('cat ' + path) + ';' })
+  , code     = contents.join('')
+  , source   = __dirname + '/dist/cubi.js'
 
-  exec(cmd)
-})
+var ast = jsp.parse(code); // parse code and get the initial AST
+ast = jsu.ast_mangle(ast); // get a new AST with mangled names
+ast = jsu.ast_squeeze(ast); // get an AST with compression optimizations
+
+var finalCode = jsu.gen_code(ast); // compressed code here
+
+fs.writeFileSync(source, finalCode);
 
 exec('ls -l ' + __dirname + '/dist/stylesheets/*').split(/\n/).forEach(function(row) {
   var path = __dirname + '/' + row.match(/(dist.*)/)[1]

4b22021cf9b7bf47e22b8a83d87e94c6a021589a
sdepold <sascha@depold.com>
2012-08-27 20:54:21 +0200

    fixed alignment for meta menu

--- javascripts/tower-meta-menu.js
+++ javascripts/tower-meta-menu.js
@@ -19,6 +19,10 @@
 
     this.popUp.setContent(ul)
     this.popUp.render({ left: x, top: y })
+
+    if(x + this.popUp.dom.offsetWidth > document.querySelector('table').offsetWidth) {
+      this.popUp.setPosition({ left: (x - 300) })
+    }
   }
 
   TowerMetaMenu.prototype.clear = function() {

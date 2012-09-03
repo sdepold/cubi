1b9e3b05effb4ecaabc12b7e0ed13dad564c3b26
sdepold <sascha@depold.com>
2012-09-03 20:08:47 +0200

    refactored build script + base 64 encode images

--- utils/build.js
+++ utils/build.js
@@ -16,51 +16,88 @@ var exec = function(cmd, showStdOut) {
 }
 
 var targetFolder = __dirname + '/../dist'
+  , scripts      = []
 
-exec('rm -rf ' + targetFolder)
+var createTargetFolder = function() {
+  exec('rm -rf ' + targetFolder)
+  exec('mkdir -p ' + targetFolder)
+}
 
-exec('mkdir -p ' + targetFolder)
+var copySources = function() {
+  exec('cp ' + __dirname + '/../stylesheets/screen.css ' + targetFolder + '/cubi.css')
+  exec('cp -rf ' + __dirname + '/../index.html ' + targetFolder + '/index.html')
+}
 
-exec('cp -rf ' + __dirname + '/../images ' + targetFolder + '/images')
-exec('cp ' + __dirname + '/../stylesheets/screen.css ' + targetFolder + '/cubi.css')
-exec('cp -rf ' + __dirname + '/../index.html ' + targetFolder + '/index.html')
+var modifyIndexHtml = function() {
+  var indexHTML = exec('cat ' + targetFolder + '/index.html')
 
-var indexHTML = exec('cat ' + targetFolder + '/index.html')
-  , scripts   = []
-
-indexHTML = indexHTML.split('\n').map(function(row) {
-  if(row.indexOf('<link') !== -1) {
-    return row.replace('stylesheets/screen.css', 'cubi.css')
-  } else if(row.indexOf('<script') === -1) {
-    return row
-  } else {
-    if(process.env.SKIP_PREFIXFREE && (row.indexOf('prefixfree') !== -1)) {
-      return null
+  indexHTML = indexHTML.split('\n').map(function(row) {
+    if(row.indexOf('<link') !== -1) {
+      return row.replace('stylesheets/screen.css', 'cubi.css')
+    } else if(row.indexOf('<script') === -1) {
+      return row
     } else {
-      scripts.push(row.match(/src="(.*)"/)[1])
-      return null
+      if(process.env.SKIP_PREFIXFREE && (row.indexOf('prefixfree') !== -1)) {
+        return null
+      } else {
+        scripts.push(row.match(/src="(.*)"/)[1])
+        return null
+      }
     }
-  }
-}).filter(function(row) {
-  return row !== null
-}).join('\n').replace('</head>', '  <script src="cubi.js"></script>\n  </head>')
+  }).filter(function(row) {
+    return row !== null
+  }).join('\n').replace('</head>', '  <script src="cubi.js"></script>\n  </head>')
 
-fs.writeFileSync(targetFolder + '/index.html', indexHTML)
+  fs.writeFileSync(targetFolder + '/index.html', indexHTML)
+}
 
-var paths    = scripts.map(function(file) { return __dirname + '/../' + file })
-  , contents = paths.map(function(path) { return exec('cat ' + path) + ';' })
-  , code     = contents.join('')
-  , source   = targetFolder + '/cubi.js'
+var compressJsFiles = function() {
+  var paths    = scripts.map(function(file) { return __dirname + '/../' + file })
+    , contents = paths.map(function(path) { return exec('cat ' + path) + ';' })
+    , code     = contents.join('')
+    , source   = targetFolder + '/cubi.js'
 
-var ast = jsp.parse(code)
-ast = jsu.ast_mangle(ast)
-ast = jsu.ast_squeeze(ast)
+  var ast = jsp.parse(code)
+  ast = jsu.ast_mangle(ast)
+  ast = jsu.ast_squeeze(ast)
 
-var finalCode = jsu.gen_code(ast); // compressed code here
+  var finalCode = jsu.gen_code(ast); // compressed code here
 
-fs.writeFileSync(source, finalCode);
+  fs.writeFileSync(source, finalCode);
+}
 
-var yuiCall = 'node ' + __dirname + '/../node_modules/yuicompressor/nodejs/cli.js'
-  , cssPath = targetFolder + '/cubi.css'
+var base64EncodeImages = function() {
+  var path = targetFolder + '/cubi.css'
+    , css  = exec('cat ' + path)
 
-exec(yuiCall + ' --type css -o ' + cssPath + ' ' + cssPath)
+  css = css.split('\n').map(function(row) {
+    if(row.indexOf('url(') === -1) {
+      return row
+    } else {
+      return row.replace(/url\(['"](.*?)['"]\)/, function(match, path) {
+        var imageData = fs.readFileSync(__dirname + '/' + path)
+          , prefix    = 'data:image/gif;base64,'
+          , base64    = new Buffer(imageData, 'binary').toString('base64')
+          , data      = prefix + base64
+
+        return "url('" + data + "')"
+      })
+    }
+  }).join('\n')
+
+  fs.writeFileSync(path, css);
+}
+
+var compresCssFiles = function() {
+  var yuiCall = 'node ' + __dirname + '/../node_modules/yuicompressor/nodejs/cli.js'
+    , cssPath = targetFolder + '/cubi.css'
+
+  exec(yuiCall + ' --type css -o ' + cssPath + ' ' + cssPath)
+}
+
+createTargetFolder()
+copySources()
+modifyIndexHtml()
+compressJsFiles()
+base64EncodeImages()
+compresCssFiles()

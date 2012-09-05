584b6b5c8c10cc08a034bf2f0b8f24aceb911bda
sdepold <sascha@depold.com>
2012-09-05 09:53:40 +0200

    fixed boot script

--- javascripts/boot.js
+++ javascripts/boot.js
@@ -1,18 +1,19 @@
-var loadedMethod = window.addEventListener || window.attachEvent
+var readyStateCheckInterval = setInterval(function() {
+  if (document.readyState === "complete") {
+    clearInterval(readyStateCheckInterval)
 
-loadedMethod('load', function() {
-  var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight
+    var w=window,d=document,e=d.documentElement,g=d.getElementsByTagName('body')[0],x=w.innerWidth||e.clientWidth||g.clientWidth,y=w.innerHeight||e.clientHeight||g.clientHeight
+    var cellWidth  = 24
+      , cellHeight = 28
+      , cols       = ~~((x - 10) / cellWidth)
+      , rows       = ~~((y - 30) / cellHeight)
 
-  var cellWidth  = 24
-    , cellHeight = 28
-    , cols       = ~~((x - 10) / cellWidth)
-    , rows       = ~~((y - 30) / cellHeight)
+    if(cols < 20) {
+      document.body.className = document.body.className.split(' ').concat(['reduced']).join(' ')
+    }
 
-  if(cols < 20) {
-    document.body.className = document.body.className.split(' ').concat(['reduced']).join(' ')
+    game = new Game('body', { cols: cols, rows: rows }).render()
+
+    document.getElementById('meta-data').style.width = (document.querySelector('table').offsetWidth - 10) + 'px'
   }
-
-  game = new Game('body', { cols: cols, rows: rows }).render()
-
-  document.getElementById('meta-data').style.width = (document.querySelector('table').offsetWidth - 10) + 'px'
-}, false);
+}, 10)

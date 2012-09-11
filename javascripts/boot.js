88a4c32880919bfc6d622ae20abb8977e1e11f17
sdepold <sascha@depold.com>
2012-09-11 21:04:05 +0200

    refactored usage of query selector

--- javascripts/boot.js
+++ javascripts/boot.js
@@ -14,6 +14,9 @@ var readyStateCheckInterval = setInterval(function() {
 
     game = new Game('#game', { cols: cols, rows: rows }).render()
 
-    document.getElementById('meta-data').style.width = (document.querySelector('#game').offsetWidth - 10) + 'px'
+    var gameDom     = document.getElementById('game')
+      , metaDataDom = document.getElementById('meta-data')
+
+    metaDataDom.style.width = (gameDom.offsetWidth - 10) + 'px'
   }
 }, 10)
